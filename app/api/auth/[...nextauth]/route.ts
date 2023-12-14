384a55161319c91ef5b16f2231865e72045ab8fc
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-14 07:55:19 +0200

    - fix/api: jwt payload contains username now Used to be a 'name' field that was undefined. Changed that with prisma custom callbacks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -25,13 +25,26 @@ export const authOptions: NextAuthOptions = {
         const isPasswordValid = await compare(credentials.password, user.hash);
         if (!isPasswordValid) return null;
         return {
-            id: user.id,
-            email: user.email,
-            username: user.username
-        }
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
       },
     }),
   ],
+  // by default, NextAuth will only contain "name" and "email" in the payload
+  // I wanted a "username" field, so I did those custom callbacks
+  callbacks: {
+    async jwt({ token, user, account, profile }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      return {
+        expires: session.expires,
+        user: { email: session.user?.email, username: token.username },
+      };
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
