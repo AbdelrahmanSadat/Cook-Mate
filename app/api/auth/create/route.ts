abd0b09093951c7990dcae819431a30aa806da89
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-14 06:49:03 +0200

    - feat/api: create user route

---
+++ app/api/auth/create/route.ts
@@ -0,0 +1,35 @@
+// We impot our prisma client
+// Prisma will help handle and catch errors
+import { Prisma } from "@prisma/client";
+import { hashSync } from "bcrypt";
+import prisma from "@/lib/prisma";
+
+// We hash the user entered password using bcrypt
+export const hashPassword = (string: string) => {
+  return hashSync(string, 10)
+};
+// function to create user in our database
+export async function POST(request: Request) {
+  let errors = [];
+  const body = await request.json();
+  const { username, email, password } = body;
+ 
+  if (password.length < 6) {
+    errors.push("password length should be more than 6 characters");
+    return Response.json("Password must be 6 characters or longer", {status: 400})
+  }
+  try {
+    const user = await prisma.user.create({
+      data: { username, email, hash: hashPassword(body.password) },
+    });
+    return Response.json(user, {status: 201})
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return Response.json({message: e.message}, {status: 400})
+      }
+      return Response.json({message: e.message}, {status: 400})
+    }
+    console.log(e)
+  }
+}
