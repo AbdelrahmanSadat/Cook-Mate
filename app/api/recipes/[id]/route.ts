c7f5d0e1ed43852085089084d6a3f1770edb7146
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-16 01:59:20 +0200

    - feat/api: added creator with Recipe by ID route

--- app/api/recipes/[id]/route.ts
+++ app/api/recipes/[id]/route.ts
@@ -6,11 +6,10 @@ import { authOptions } from '../../auth/[...nextauth]/authOptions';
 export const dynamic = 'force-dynamic';
 
 // route params will always be string, even if cast as id:number ?
-// TODO?: See this: https://www.prisma.io/blog/nestjs-prisma-validation-7D056s1kOla1#perform-input-validation
-// for parseInt pipe on dynamic routes
+// TODO?: See this: https://www.prisma.io/blog/nestjs-prisma-validation-7D056s1kOla1#perform-input-validation for parseInt pipe on dynamic routes
 // TODO: route validation for params
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-  let res = await prisma.recipe.findFirstOrThrow({ where: { id: +params.id } });
+  let res = await prisma.recipe.findFirstOrThrow({ where: { id: +params.id }, include: {creator: true} });
   return Response.json(res);
 }
 
