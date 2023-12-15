1e9d057586dbd36d9a404ffdbf0b59e3fcef973f
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-16 01:52:21 +0200

    - chore: added fetcher function wrapper for SWR use

---
+++ lib/fetcher.ts
@@ -0,0 +1,2 @@
+// "Parameters" fetches the param types of a function. And since this is a wrapper, this is all we need
+export const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
