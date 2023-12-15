0d477300bb43626ef03ff32d0128e239b4a129fa
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-16 01:59:44 +0200

    - hotfix!/refactor: refactored FoodCard to take props

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -1,21 +1,21 @@
 import { Card, Image, Text, Badge, Button, Group, CardSection } from '@mantine/core';
+import { Recipe } from '@prisma/client';
+import Link from 'next/link';
 
-export default function FoodCard() {
+export default function FoodCard({ recipe }: { recipe: Recipe }) {
   return (
     <>
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <CardSection>
           <Image
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+            src="https://www.fooddolls.com/wp-content/uploads/2021/10/Koshari-2-500x375.jpg"
             height={160}
             alt="Norway"
           />
         </CardSection>
 
         <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500} className="bg-red-100">
-            Koshari
-          </Text>
+          <Text fw={500}>{recipe.title}</Text>
           <div>
             <Badge color="pink">Egyptian Food</Badge>
             <Badge color="gold">3/5 Stars</Badge>
@@ -23,19 +23,14 @@ export default function FoodCard() {
         </Group>
 
         <Text size="sm" c="dimmed">
-          How to make Koshari: Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-          molestiae necessitatibus maxime sint ipsam totam deleniti autem pariatur corrupti aperiam
-          inventore numquam quia dolores magnam vitae veritatis voluptas illo quae perspiciatis
-          fugit at modi accusamus doloribus. Dolorum, illum ad, velit provident facilis nemo
-          reprehenderit laborum consequatur cumque, temporibus facere beatae. At nisi alias itaque
-          nulla illum autem optio mollitia minus obcaecati libero dolorem aperiam quaerat saepe,
-          quis quisquam, cupiditate suscipit rerum? Minus neque labore deserunt, a voluptate placeat
-          omnis tempora.
+          {recipe.description}
         </Text>
 
-        <Button color="blue" fullWidth mt="md" radius="md">
-          See How To Make
-        </Button>
+        <Link href={`recipes/${recipe.id}`}>
+          <Button color="blue" fullWidth mt="md" radius="md">
+            See How To Make
+          </Button>
+        </Link>
       </Card>
     </>
   );
