778d54943bc3124d7157bbed231a27bc78915699
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-16 01:56:27 +0200

    - feat: fetching data in main page for recipes

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,22 @@
 import FoodCard from '@/components/FoodCard';
 import prisma from '@/lib/prisma';
-
-async function getData() {
-  // const res = await fetch("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8");
-  const res = await prisma.recipe.findMany();
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // if (!res.ok) {
-  //   // This will activate the closest `error.js` Error Boundary
-  //   throw new Error('Failed to fetch data');
-  // }
-
-  return res;
-}
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/authOptions';
+import { Recipe } from '@prisma/client';
 
 export default async function HomePage() {
-  const data = await getData();
-  console.log(data);
-  // I am only slightly confused about the ramafications of using await like this before the render.
-  // I guess the NextJS fetch api is optimized and memoized and all that shit?
+  const recipesRes = await fetch(`${process.env.domain}/api/recipes`);
+  const recipes = await recipesRes.json();
+
+  const session = await getServerSession(authOptions);
+  console.log('Server session from Home Page', session);
 
   return (
     <>
       <div className="grid grid-cols-4 gap-4 p-4">
-        <FoodCard></FoodCard>
-        <FoodCard></FoodCard>
-        <FoodCard></FoodCard>
-        <FoodCard></FoodCard>
+        {recipes.map((recipe: Recipe) => (
+          <FoodCard key={recipe.id} recipe={recipe}></FoodCard>
+        ))}
       </div>
     </>
   );
