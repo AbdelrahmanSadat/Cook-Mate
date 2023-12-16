dfb9e6757606d25061361c6769cfefaedb0a0a90
Abdelrahman Sadat <abdelrahman.sadat@ithraa.sa>
2023-12-16 22:01:02 +0200

    - feat: minimum styling in register form

--- app/recipes/create/page.tsx
+++ app/recipes/create/page.tsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/navigation';
 export default function CreateRecipe() {
   const router = useRouter();
   return (
-    <Container className="h-full flex flex-wrap justify-center content-center">
+    <Container className="h-[90vh] flex flex-wrap justify-center content-center">
       <Formik
         initialValues={{ title: '', description: '', ingredients: '', steps: '' }}
         onSubmit={async (values) => {
@@ -24,32 +24,32 @@ export default function CreateRecipe() {
         }}
       >
         <Form>
-          <Group>
+          <Group className="flex justify-center conten mt-3">
             <label htmlFor="title">Title</label>
             <Field name="title" type="text"></Field>
             <ErrorMessage name="title"></ErrorMessage>
           </Group>
 
-          <Group>
+          <Group className="flex justify-center mt-3">
             <label htmlFor="description">Description</label>
             <Field name="description" type="text" as="textarea"></Field>
             <ErrorMessage name="description"></ErrorMessage>
           </Group>
 
-          <Group>
+          <Group className="flex justify-center mt-3">
             <label htmlFor="ingredients">Ingredients</label>
             <Field name="ingredients" type="text" as="textarea"></Field>
             <ErrorMessage name="ingredients"></ErrorMessage>
           </Group>
 
-          <Group>
+          <Group className="flex justify-center mt-3">
             <label htmlFor="steps">Steps</label>
             <Field name="steps" type="text" as="textarea"></Field>
             <ErrorMessage name="steps"></ErrorMessage>
           </Group>
 
-          <Group className="flex justify-center mt-3">
-            <button type="submit">Register</button>
+          <Group className="flex justify-end mt-3">
+            <button type="submit">Create</button>
           </Group>
         </Form>
       </Formik>
